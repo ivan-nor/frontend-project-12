f93ebbcb628565b78ef0eb628b582f33ea08dc34
Ivan Nor <ivan.norik@gmail.com>
2024-02-29 15:55:54 +0300

    add jwt autorization

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,51 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useLocation
+} from 'react-router-dom'
 import axios from 'axios'
+import PropTypes from 'prop-types'
+
+import { Button, Navbar, Nav } from 'react-bootstrap'
+import useAuth from '../hooks/index.jsx'
+import AuthContext from '../contexts/index.jsx'
 import '../assets/App.css'
-import Login from './Login'
+import LoginPage from './LoginPage'
+import PublicPage from './PublicPage'
 import ErrorPath from './ErrorPath'
 import MainPage from './MainPage'
-import Chat from './Chat'
+import ChatPage from './ChatPage'
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const logIn = () => setLoggedIn(true)
+  const logOut = () => {
+    localStorage.removeItem('userId')
+    // console.log('LOGOUT')
+    setLoggedIn(false)
+  }
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth()
+  const location = useLocation()
+
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  )
+}
 
 function App () {
   // useEffect(() => {
@@ -24,18 +63,70 @@ function App () {
   //   })
   // })
 
+  const AuthButton = () => {
+    const auth = useAuth()
+    const location = useLocation()
+
+    return (
+      auth.loggedIn
+        ? <Button onClick={auth.logOut}>Log out</Button>
+        : <Button as={Link} to="/login" state={{ from: location }}>Log in</Button>
+    )
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} >
-          <Route index element={<div>No page is selected.</div> } />
-          <Route path="login" element={<Login />} />
-          <Route path="chat" element={<Chat />} />
-          <Route path="*" element={<ErrorPath />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <Router>
+        <Navbar bg="light" expand="lg" className='p-2'>
+          <Navbar.Brand as={Link} to="/">Secret Place</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/public">Public page</Nav.Link>
+            <Nav.Link as={Link} to="/private">Private page</Nav.Link>
+          </Nav>
+          <AuthButton />
+        </Navbar>
+
+        <div className="container p-3">
+          <h1 className="text-center mt-5 mb-4">Welcome to the secret place###</h1>
+          <Routes>
+            <Route path="/" element={null} />
+            <Route path="/public" element={<PublicPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/private"
+              element={(
+                <PrivateRoute>
+                  <ChatPage />
+                </PrivateRoute>
+              )}
+            />
+          </Routes>
+        </div>
+
+      </Router>
+    </AuthProvider>
   )
+
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route path="/" element={<MainPage />} >
+  //         <Route index element={<div>No page is selected.</div> } />
+  //         <Route path="login" element={<Login />} />
+  //         <Route path="chat" element={<Chat />} />
+  //         <Route path="*" element={<ErrorPath />} />
+  //       </Route>
+  //     </Routes>
+  //   </BrowserRouter>
+  // )
+}
+
+AuthProvider.propTypes = {
+  children: PropTypes.element.isRequired
+}
+
+PrivateRoute.propTypes = {
+  children: PropTypes.element.isRequired
 }
 
 export default App
---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,118 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect, useRef } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useFormik, ErrorMessage, Field, FormikProvider } from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+import useAuth from '../hooks'
+
+const LoginPage = () => {
+  const [authFailed, setAuthFailed] = useState(false)
+  const location = useLocation()
+  const navigate = useNavigate()
+  const auth = useAuth()
+  const inputRef = useRef()
+  // useEffect(() => {
+  //   inputRef.current.focus()
+  // }, [])
+  // useEffect(() => console.log('authFailed', authFailed), [authFailed])
+
+  const SignupSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(2, 'Минимум 2 буквы')
+      .max(10, 'Максимум 10 букв')
+      .required('Обязательное поле'),
+    password: Yup.string()
+      .min(2, 'Минимум 2 буквы')
+      .max(10, 'Максимум 10 букв')
+      .required('Обязательное поле')
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validationSchema: SignupSchema,
+    onBlur: (e, param) => console.log('bluuuuurr', e, param),
+    onSubmit: async (values) => {
+      console.log(JSON.stringify(values, null, 2))
+      setAuthFailed(false)
+
+      try {
+        const response = await axios.post('/api/v1/login', values)
+        localStorage.setItem('userId', JSON.stringify(response.data))
+        auth.logIn()
+        const { from } = location.state
+        navigate(from)
+        console.log('LOGIN response', response.data)
+      } catch (err) {
+        formik.setSubmitting(false)
+        if (err.isAxiosError && err.response.status === 401) {
+          setAuthFailed(true)
+          // inputRef.current.select() // выделение и фокус на инпуте после ошибки
+          return
+        }
+        throw err
+      }
+    }
+  })
+
+  const handleFocus = () => {
+    // console.log('focus')
+    if (authFailed) {
+      setAuthFailed(false)
+      formik.resetForm()
+    }
+  }
+
+  return (
+    <div className="container-fluid">
+      <div className="row justify-content-center pt-5">
+        <div className="col-sm-4">
+        <div className='text-danger'>{authFailed ? 'the username or password is incorrect' : ''}</div>
+          <Form onSubmit={formik.handleSubmit} className="p-3" name="form">
+            <fieldset>
+              <Form.Group>
+                <Form.Label htmlFor="username">Username</Form.Label>
+                <Form.Control
+                  onFocus={handleFocus}
+                  onChange={formik.handleChange}
+                  value={formik.values.username}
+                  placeholder="username"
+                  name="username"
+                  id="username"
+                  autoComplete="username"
+                  isInvalid={formik.errors.username || authFailed}
+                  required
+                  ref={inputRef}
+                />
+                <Form.Control.Feedback type="invalid">{formik.errors.username ? formik.errors.username : null}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  onFocus={handleFocus}
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
+                  placeholder="password"
+                  name="password"
+                  id="password"
+                  autoComplete="current-password"
+                  isInvalid={formik.errors.password || authFailed}
+                  required
+                />
+                <Form.Control.Feedback type="invalid">{formik.errors.password ? formik.errors.password : ''}</Form.Control.Feedback>
+              </Form.Group>
+              <Button type="submit" variant="outline-primary">Submit</Button>
+            </fieldset>
+          </Form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
