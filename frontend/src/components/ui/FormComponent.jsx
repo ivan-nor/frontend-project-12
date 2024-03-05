f224f83fff741cc320da2a30ba2d9662da2de09a
Ivan Nor <ivan.norik@gmail.com>
2024-03-05 13:06:19 +0300

    continue fix form

--- frontend/src/components/ui/FormComponent.jsx
+++ frontend/src/components/ui/FormComponent.jsx
@@ -1,38 +1,34 @@
 /* eslint-disable react/prop-types */
 // import { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, FloatingLabel } from 'react-bootstrap'
 import InputComponent from './InputComponent'
 
 const FormComponent = ({ formik, handleFocus, authFailed }) => {
   return (
     <>
       <div className='text-danger'>{authFailed ? 'the username or password is incorrect' : ''}</div>
-      <Form onSubmit={formik.handleSubmit} className="p-3" name="form">
-        <fieldset>
-          <Form.Group className='position-relative'>
-            <Form.Label htmlFor="username">Username</Form.Label>
-            <InputComponent
-              name={'username'}
-              isInvalid={formik.errors.username || authFailed}
-              value={formik.values.username}
-              handleFocus={handleFocus}
-              handleChange={formik.handleChange}
-            />
-            <Form.Control.Feedback type="invalid">{formik.errors.username ? formik.errors.username : null}</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label htmlFor="password">Password</Form.Label>
-            <InputComponent
-              name={'password'}
-              handleFocus={handleFocus}
-              handleChange={formik.handleChange}
-              value={formik.values.password}
-              isInvalid={formik.errors.password || authFailed}
-            />
-            <Form.Control.Feedback type="invalid">{formik.errors.password ? formik.errors.password : ''}</Form.Control.Feedback>
-          </Form.Group>
+      <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
+            <FloatingLabel htmlFor="username" label='Username' controlId='username' className='mb-3'>
+              <InputComponent
+                name={'username'}
+                isInvalid={formik.errors.username || authFailed}
+                value={formik.values.username}
+                handleFocus={handleFocus}
+                handleChange={formik.handleChange}
+              />
+              <Form.Control.Feedback type="invalid">{formik.errors.username ? formik.errors.username : null}</Form.Control.Feedback>
+            </FloatingLabel>
+            <FloatingLabel controlId='password' label='Password' htmlFor="password" className='mb-4'>
+              <InputComponent
+                name={'password'}
+                handleFocus={handleFocus}
+                handleChange={formik.handleChange}
+                value={formik.values.password}
+                isInvalid={formik.errors.password || authFailed}
+              />
+              <Form.Control.Feedback type="invalid">{formik.errors.password ? formik.errors.password : ''}</Form.Control.Feedback>
+            </FloatingLabel>
           <Button type="submit" variant="outline-primary">Submit</Button>
-        </fieldset>
       </Form>
     </>
   )
