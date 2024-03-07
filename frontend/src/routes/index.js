2e30db0774d435a1a157ea02f9f5e972d32c8f82
Ivan Nor <ivan.norik@gmail.com>
2024-03-07 12:23:13 +0300

    add messages path

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,5 +2,6 @@ const apiPath = '/api/v1'
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
-  channelsPath: () => [apiPath, 'channels'].join('/')
+  channelsPath: () => [apiPath, 'channels'].join('/'),
+  messagesPath: () => [apiPath, 'messages'].join('/')
 }
