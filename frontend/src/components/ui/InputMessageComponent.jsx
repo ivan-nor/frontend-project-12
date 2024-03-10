c8e4a53bce7f3059e54264899cc526aa840d17cf
Ivan Nor <ivan.norik@gmail.com>
2024-03-10 14:09:00 +0300

    update inputMessage to stupid component

--- frontend/src/components/ui/InputMessageComponent.jsx
+++ frontend/src/components/ui/InputMessageComponent.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
+/* eslint-disable react/prop-types */
+// import { useState } from 'react'
 import { Form, Button, InputGroup } from 'react-bootstrap'
+// import { useDispatch } from 'react-redux'
+// import { addMessage } from '../../slices/messagesSlice'
 
-const InputMessageComponent = () => {
-  const [value, setValue] = useState('')
+const InputMessageComponent = ({ handleChange, handleSubmit, text }) => {
+  // const [value, setValue] = useState('')
+  // const dispatch = useDispatch()
 
-  const handleSubmit = (e) => { // dispatch(sendMessage(value))
-    e.preventDefault()
-    console.log('SEND MESSAGE', value)
-    setValue('')
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   const newMessage = { body: value, channelId: id, username: 'admin' }
+  //   console.log('SEND MESSAGE id', id, value)
+  //   dispatch(addMessage(newMessage))
+  //   setValue('')
+  // }
 
   return (
       <Form name='inputMessage' onSubmit={handleSubmit} className='p-2 align-content-end'>
         <InputGroup>
           <Form.Control
             type='text'
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={text}
+            onChange={handleChange}
             placeholder='введите сообщение'
             className='d-inline'
           />
-          <Button type='submit' inline>отправить</Button>
+          <Button type='submit' className='d-inline'>Отправить</Button>
         </InputGroup>
       </Form>
   )
