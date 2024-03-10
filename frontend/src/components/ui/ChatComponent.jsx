f68555e1d1ff61888e1c8328db6a966115ba1b2f
Ivan Nor <ivan.norik@gmail.com>
2024-03-10 14:12:20 +0300

    extract logic

--- frontend/src/components/ui/ChatComponent.jsx
+++ frontend/src/components/ui/ChatComponent.jsx
@@ -1,53 +1,75 @@
 /* eslint-disable react/prop-types */
 import { Row, Col, Tab, Nav, Button, ButtonGroup, Dropdown } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 // import ChannelsComponent from './ChannelsComponent'
 import ChatWindow from './ChatWindow'
 import InputMessageComponent from './InputMessageComponent'
+import { selectors as channelsSelectors, addChannel } from '../../slices/channelsSlice'
+import { selectors as messagesSelectors } from '../../slices/messagesSlice'
 
 // ДОБАВИТЬ обработку киликов по кнопке канала
 
-const Buttons = () => (
-  <ButtonGroup aria-label="Basic example">
-      <Dropdown as={ButtonGroup}>
-      <Button variant="success" size='xl'>Split Button</Button>
-
-      <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
+const Buttons = ({ name, isRemovable }) => (
+  <ButtonGroup aria-label="channel buttons">
+    <Dropdown as={ButtonGroup}>
+      <Button variant="success" size='xl'>{name}</Button>
+      { isRemovable && (
+        <>
+          <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Переименовать</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Удалить</Dropdown.Item>
+          </Dropdown.Menu>
+        </>
+      )}
     </Dropdown>
-    </ButtonGroup>
+  </ButtonGroup>
 )
 
-const ChatComponent = ({ channels, messages }) => {
+const ChatComponent = () => {
+  const dispatch = useDispatch()
+  const channels = useSelector(channelsSelectors.selectAll)
+  const messages = useSelector(messagesSelectors.selectAll)
+
+  console.log('CHAT COMP channels messages', channels, messages)
+
   return (
-    <Tab.Container id="left-tabs-example" defaultactivekey="first" fluid>
+    <Tab.Container id="left-tabs-example" defaultActiveKey={1} fluid>
       <Row>
         <Col sm={3}>
+          <Button variant='secondary' onClick={() => dispatch(addChannel('TEMP'))}>+</Button>
           <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link as='div' eventKey="first" >
-                <Buttons />
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as='div' eventKey="second">Tab 2</Nav.Link>
-            </Nav.Item>
+            {channels.map((channel) => (
+              <Nav.Item key={channel.id}>
+                <Nav.Link as='div' eventKey={channel.id} >
+                  <Buttons name={channel.name} isRemovable={channel.removable} id={channel.id} />
+                </Nav.Link>
+              </Nav.Item>
+            ))}
           </Nav>
         </Col>
         <Col sm={9} className='d-flex flex-column justify-content-between'>
-          <Tab.Content defaultactivekey='first'>
-            <Tab.Pane eventKey="first" as='div'>
-              <ChatWindow channels={channels[0]}/>
+          <Tab.Content defaultActiveKey={channels[0]?.id}>
+            {/* <Tab.Pane eventKey="first" as='div'>
+              <ChatWindow channel={channels[0]}/>
+              <InputMessageComponent channel={channels[0]}/>
             </Tab.Pane>
             <Tab.Pane eventKey="second" as='div'>
-              <ChatWindow channels={channels[1]} />
-            </Tab.Pane>
+              <ChatWindow id={channels[1]} />
+              <InputMessageComponent id={channels[1]} />
+            </Tab.Pane> */}
+            {channels.map((channel) => {
+              const currentMessages = useSelector((state) => messagesSelectors.selectById(state, channel.id))
+              console.log('CHAT in map', currentMessages)
+              return (
+                <Tab.Pane eventKey={channel.id} as='div' key={channel.id}>
+                  <ChatWindow id={channel.id} messages={currentMessages}/>
+                  <InputMessageComponent id={channel.id}/>
+                </Tab.Pane>
+              )
+            }
+            )}
           </Tab.Content>
-          <InputMessageComponent />
         </Col>
       </Row>
     </Tab.Container>
--- frontend/src/components/ui/ChatWindow.jsx
+++ frontend/src/components/ui/ChatWindow.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from 'react'
+// import { useSelector } from 'react-redux'
+// import { selectors } from '../../slices/messagesSlice'
 
-const ChatWindow = (props) => {
-  console.log(props.channels)
+const ChatWindow = ({ id, messages }) => {
+  // const messages = useSelector((state) => selectors.selectById(state, id))
+  // const currentMessages = messages.filter(({ channelId }) => channelId === id)
+
+  useEffect(() => console.log('CHAT WINDOW', messages))
+
   return (
-    <div>CHAT WINDOW {props?.channels?.id} | {props?.channels?.name}</div>
-    // <div>CHAT WINDOW </div>
+    <>
+      <h1>CHAT WINDOW {id}</h1>
+      <ul>
+        {messages.map((message) => (
+          <li key={message.id}>USER: {message.username} | MESSAGE: {message.body}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
