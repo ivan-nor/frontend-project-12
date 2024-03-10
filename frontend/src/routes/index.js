d8e7714a814a0d2cf2efb610093ebda996a735d8
Ivan Nor <ivan.norik@gmail.com>
2024-03-10 14:07:57 +0300

    update routes paths

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,6 +3,8 @@ const apiPath = '/api/v1'
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
   channelsPath: () => [apiPath, 'channels'].join('/'),
-  channelPath: (id) => [apiPath, 'channels', ':', id].join('/'),
-  messagesPath: () => [apiPath, 'messages'].join('/')
+  channelPath: (id) => [apiPath, 'channels', id].join('/'),
+  messagesPath: () => [apiPath, 'messages'].join('/'),
+  messagePath: (id) => [apiPath, 'messages', id].join('/'),
+  signupPatn: () => [apiPath, 'signup'].join('/')
 }
