0a60c8c54b83c917d46961cd5b31c0b80f623f70
Ivan Nor <ivan.norik@gmail.com>
2024-03-18 20:14:22 +0300

    set dark theme

--- frontend/src/components/ui/HeaderComponent.jsx
+++ frontend/src/components/ui/HeaderComponent.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/prop-types */
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const HeaderComponent = ({ children, link, route }) => {
   return (
-    <Navbar bg="white" expand="lg" className='p-2'>
-      <Navbar.Brand as={link} to={route}>Hexlet chat</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link as={link} to={route}>Chat</Nav.Link>
-      </Nav>
-      {children}
-    </Navbar>
+    <Container fluid='xl'>
+      <Navbar bg="dark" expand="lg" className='p-2 rounded-bottom border border-top-0' data-bs-theme="dark">
+        <Navbar.Brand as={link} to={route}>Hexlet chat</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={link} to={route}>Chat</Nav.Link>
+        </Nav>
+        {children}
+      </Navbar>
+    </Container>
   )
 }
 
