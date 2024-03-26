0da747ea55b2f81678981b85748951f076b032c2
Ivan Nor <ivan.norik@gmail.com>
2024-03-26 22:43:40 +0300

    fix input label error render

--- frontend/src/components/ui/InputComponent.jsx
+++ frontend/src/components/ui/InputComponent.jsx
@@ -1,30 +1,32 @@
 /* eslint-disable react/prop-types */
 import { useTranslation } from 'react-i18next'
-import { Form, FloatingLabel } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 
+// #TODO переименовать в AuthInput
 const InputComponent = ({ name, value, handleChange, handleFocus, isInvalid, handleBlur, type, error }) => {
   const { t } = useTranslation()
 
   return (
-        <FloatingLabel label={t(`${type}.${name}`)} htmlFor={name} className='mb-3'>
-          <Form.Control
-            onFocus={handleFocus}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            isInvalid={isInvalid}
+    <Form.Group className="form-floating mb-3">
+      <Form.Control
+        onFocus={handleFocus}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        isInvalid={isInvalid}
 
-            // type={name.toLowerCase().includes('password') ? 'password' : 'text'} // #TODO тип поля чтобы скрывать ввод пароля, на будущее
-            className='d-block'
-            value={value}
-            placeholder={t(`${type}.${name}`)}
-            // name={t(`${type}.${name}`)}
-            id={name}
+        // type={name.toLowerCase().includes('password') ? 'password' : 'text'} // #TODO тип поля чтобы скрывать ввод пароля, на будущее
+        className='d-block'
+        value={value}
+        placeholder={t(`${type}.${name}`)}
+        name={name}
+        id={name}
 
-            autoComplete={name}
-            required
-          />
-          <Form.Control.Feedback tooltip type='invalid'>{error}</Form.Control.Feedback>
-        </FloatingLabel>
+        autoComplete={name}
+        required
+      />
+      <Form.Label htmlFor={name}>{t(`${type}.${name}`)}</Form.Label>
+      <Form.Control.Feedback tooltip type='invalid'>{error}</Form.Control.Feedback>
+    </Form.Group>
   )
 }
 
