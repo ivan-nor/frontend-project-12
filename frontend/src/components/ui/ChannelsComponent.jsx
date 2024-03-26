2f0cf61dfccfe5364bb29c4a24c43446f5e444aa
Ivan Nor <ivan.norik@gmail.com>
2024-03-26 22:43:06 +0300

    fix channels list

--- frontend/src/components/ui/ChannelsComponent.jsx
+++ frontend/src/components/ui/ChannelsComponent.jsx
@@ -1,21 +1,11 @@
 /* eslint-disable react/prop-types */
-import { ListGroup, Col, Button, Dropdown, Row } from 'react-bootstrap'
+import { Col, Button, Dropdown, Row, ButtonGroup } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 
-const getChannelName = ({ removable, name }) => removable ? name : `# ${name}`
+// const getChannelName = ({ removable, name }) => removable ? name : `# ${name}`
 
 const ChannelsComponent = ({ channels, activeId, setActiveId, showModal }) => {
   const { t } = useTranslation()
-
-  const handleRenameChannel = (channel) => {
-    showModal('renaming', channel)
-    setActiveId(channel.id)
-  }
-
-  const handleRemoveChannel = (channel) => {
-    showModal('removing', channel)
-    setActiveId()
-  }
 
   return (
     <>
@@ -27,33 +17,51 @@ const ChannelsComponent = ({ channels, activeId, setActiveId, showModal }) => {
           <Button variant='info' onClick={() => showModal('adding')}>{t('channels.add')}</Button>
         </Col>
       </Row>
-      <ListGroup variant="flush" className='d-flex gap-2 nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block gap-2'>
-        {channels.map((channel) => (
-          <>
-            <ListGroup.Item
-              key={channel.id}
-              active={activeId === channel.id}
-              onClick={() => setActiveId(channel?.id)}
-              action
-              as='button'
-              role='button'
-              variant='info'
-              className='d-flex justify-content-between align-items-center border border-opacity-50 rounded-pill w-100'
-            >
-              {getChannelName(channel)}
-              { channel.removable && (
-                <Dropdown>
-                  <Dropdown.Toggle id="dropdown-basic-button" title='' className='py-0 border-0' />
-                  <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => handleRenameChannel(channel)}>{t('channels.rename')}</Dropdown.Item>
-                    <Dropdown.Item onClick={() => handleRemoveChannel(channel)}>{t('channels.remove')}</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              )}
-            </ListGroup.Item>
-          </>
-        ))}
-      </ListGroup>
+
+      <ul className='nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block'>
+        {channels.map((channel) => {
+          const variant = activeId === channel.id ? 'secondary' : null
+
+          return (
+            <li key={channel.id} className="nav-item w-100">
+              {channel.removable
+                ? (
+                  <Dropdown as={ButtonGroup} className="d-flex">
+                    <Button
+                      type="button"
+                      key={channel.id}
+                      className="w-100 rounded-0 text-start text-truncate"
+                      onClick={() => setActiveId(channel?.id)}
+                      variant={variant}
+                    >
+                      <span className="me-1">#</span>
+                      {channel.name}
+                    </Button>
+                    <Dropdown.Toggle split className="flex-grow-0" variant={variant}>
+                      <span className="visually-hidden">Управление каналом</span>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item onClick={() => showModal('renaming', channel)}>{t('channels.rename')}</Dropdown.Item>
+                      <Dropdown.Item onClick={() => showModal('removing', channel)}>{t('channels.remove')}</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  )
+                : (
+                  <Button
+                    type="button"
+                    variant={variant}
+                    className="w-100 rounded-0 text-start"
+                    onClick={() => setActiveId(channel?.id)}
+                  >
+                    <span className="me-1">#</span>
+                    {channel.name}
+                  </Button>
+                  )
+              }
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
